5735f103aebf3cf95c5619ca28b402905a497dae
Victor Fernandez <fernandezvictor738@gmail.com>
2025-01-22 20:50:08 +0100

    lo que hice hoy 22/01/2025

--- ACTIVITAT_13/TIC_BCN_VF/centre/urls.py
+++ ACTIVITAT_13/TIC_BCN_VF/centre/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index , name='index'),
+    path('teachers/', views.professors , name='proff'),
+    path('students/', views.alumnat , name='students'),
 ]
--- ACTIVITAT_13/TIC_BCN_VF/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_VF/centre/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 # Create your views here.
-def index(request):
-    professor = {"name":"Roger","surname":"Sobrino","surname2":"Sobrino","mail":"rogersobrino@itic.com","role":"tutor","moduls":"M6"}
-    template = loader.get_template('index_centre.html')
-    dades = template.render({'nombre':professor["name"], 'cognom':professor["surname"],'cognom2':professor["surname2"],'correo':professor["mail"],'rol':professor["role"],'modulos':professor["moduls"]})
-    return HttpResponse(dades)
+def professors(request):
+    professor = [{"name":"Roger","surname":"Sobrino","surname2":"G","mail":"rogersobrino@itic.com","curs":"DAW","role":"tutor","moduls":"M6, M7"},
+                 {"name": "Josep", "surname": "Oriol", "surname2": "Roca", "mail": "joseporiol@gmail.com", "curs": "DAW", "role": "tutor", "moduls": "M6"}
+                 
+    ]
+    return render(request,'index_centre.html',{'nombre':professor["name"], 'cognom':professor["surname"],'cognom2':professor["surname2"],'correo':professor["mail"],'curso':professor["curs"],'rol':professor["role"],'modulos':professor["moduls"]} )
+
+def alumnat(request):
+    students = [
+        {"name": "Anna", "surname": "Martínez", "surname2": "López", "mail": "anna.martinez@itic.com", "curs": "DAW", "moduls": "M6, M8"},
+        {"name": "Carles", "surname": "García", "surname2": "Pérez", "mail": "carles.garcia@itic.com", "curs": "DAM", "moduls": "M7, M9"}
+    ]
+    return render(request, 'alumnat.html', {"students": students})
