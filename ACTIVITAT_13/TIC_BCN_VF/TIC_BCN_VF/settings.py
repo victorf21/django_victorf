f25af05d685ee4bb95a23425102d5dab05de441a
Victor Fernandez <fernandezvictor738@gmail.com>
2025-02-10 19:00:55 +0100

    lo que hice hoy

--- ACTIVITAT_13/TIC_BCN_VF/TIC_BCN_VF/settings.py
+++ ACTIVITAT_13/TIC_BCN_VF/TIC_BCN_VF/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'TIC_BCN_VF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
