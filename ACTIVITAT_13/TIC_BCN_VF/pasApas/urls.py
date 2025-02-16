44c972059e05c90969836406dbaaedb300dda3a4
Victor Fernandez <fernandezvictor738@gmail.com>
2025-02-16 23:13:15 +0100

    vistes de login sense session

--- ACTIVITAT_13/TIC_BCN_VF/pasApas/urls.py
+++ ACTIVITAT_13/TIC_BCN_VF/pasApas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('user-form/', views.user_form, name='user_form'),
+    path('login-form/', views.login_sense_sessio, name='login_form'),
 ]
--- ACTIVITAT_13/TIC_BCN_VF/pasApas/views.py
+++ ACTIVITAT_13/TIC_BCN_VF/pasApas/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .forms import PersonForm
+from .models import Usuari
+
 # Create your views here.
 def user_form(request):
     form = PersonForm()
     context = {'form':form}
     return render(request, 'form.html', context)
+
+def login_sense_sessio(request):
+    error = None
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            usuari = Usuari.objects.get(email=email)
+            if password == usuari.contrasenya:  
+                return render(request, 'index.html')
+            else:
+                error = "Credencials incorrectes."
+        except Usuari.DoesNotExist:
+            error = "Usuari no trobat."
+
+    return render(request, 'login.html', {'error': error})
